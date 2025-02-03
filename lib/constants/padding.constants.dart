573933e431d371020623419cb0017259fdb4042f
Belli <ansersonbelli@gmail.com>
2025-02-03 17:35:35 -0300

    Adding new constants for Padding and TextSizes

---
+++ lib/constants/padding.constants.dart
@@ -0,0 +1,5 @@
+abstract class AppPadding {
+  static const double kSize8 = 8;
+  static const double kSize16 = 16;
+  static const double kSize24 = 24;
+}
---
+++ lib/constants/text_sizes.constants.dart
@@ -0,0 +1,8 @@
+abstract class AppTextSize {
+  static const double textScaleFactor = 4.0;
+
+  static const double kSmall = 2 * textScaleFactor;
+  static const double kMedium = 4 * textScaleFactor;
+  static const double kLarge = 6 * textScaleFactor;
+  static const double kExtraLarge = 8 * textScaleFactor;
+}
