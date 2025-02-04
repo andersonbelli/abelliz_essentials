00f51b61ef7267c625e1c34f7d60684e32bda361
Belli <ansersonbelli@gmail.com>
2025-02-04 19:13:41 -0300

    Fixing "Unbounded" error in Modal Widget

--- lib/widgets/dialogs/show_app_modal.dart
+++ lib/widgets/dialogs/show_app_modal.dart
@@ -1,12 +1,21 @@
 import 'package:flutter/material.dart';
 
+import '../../constants/padding.constants.dart';
+import '../buttons/close_button.dart';
+
 showAppModalAbelliz(
   BuildContext context, {
   required Widget child,
+  EdgeInsets padding = const EdgeInsets.symmetric(
+    vertical: AppPadding.kSize8,
+    horizontal: AppPadding.kSize16,
+  ),
   double heightFactor = 0.9,
   bool isScrollControlled = true,
   VoidCallback? buildFunction,
   VoidCallback? onCompleteFunction,
+  Widget? title,
+  bool showCloseButton = true,
 }) =>
     showModalBottomSheet(
       context: context,
@@ -16,7 +25,27 @@ showAppModalAbelliz(
 
         return FractionallySizedBox(
           heightFactor: heightFactor,
-          child: child,
+          child: Padding(
+            padding: padding,
+            child: Column(
+              children: [
+                if (title != null || showCloseButton)
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      if (title != null) Expanded(child: title),
+                      if (showCloseButton)
+                        CloseButtonAbelliz(
+                          action: () => Navigator.pop(context),
+                        ),
+                    ],
+                  ),
+                Flexible(
+                  child: child,
+                ),
+              ],
+            ),
+          ),
         );
       },
     ).whenComplete(() => onCompleteFunction?.call());
